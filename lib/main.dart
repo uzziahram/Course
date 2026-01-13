f1c2cdd8be21b40719e53f01f08ae24aca556a85
ugenella <uzziahgenella@gmail.com>
2026-01-13 14:22:36 +0800

    Main Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -15,6 +13,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Crash Course",
       home: HomePage(),
     );
@@ -32,13 +31,33 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Text("Hello World")
-        ],
-      ),
-      
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+            children: [
+              const Text.rich(
+              TextSpan(
+                text: "Hello",
+                children: [
+                  TextSpan(
+                    text: "World",
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text: "From Flutter",
+                    style: TextStyle(fontSize: 30, color: Colors.blue)
+                  )
+                ]
+              )
+
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Hello")
+              )
+            ],
+          )
+      )
     );
   }
 }
