6f3ec9631bf0eee07d5f5d846b52a9c97047ecbd
ugenella <uzziahgenella@gmail.com>
2026-01-21 12:06:58 +0800

    Containers

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -10,27 +10,61 @@ class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Dashboard")),
-      body: Center(
-        child: Column(
+      body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Boring", style: TextStyle(fontSize: 10),),
-            IconButton(
-                onPressed: () {},
-                icon: Icon(
-                    FontAwesomeIcons.airbnb,
-                    size: 70,
-                )
-            ),
-            Text("Hello Niggas"),
-            Text("Random Number: ${getRandomNumber()}")
+            Container(
+              width: 250,
+              height: 250,
+              color: Colors.blueGrey,
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.all(20),
+              alignment: Alignment.bottomRight,
+              child: Text("Boring", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+            )
           ],
         ),
+    );
+  }
+
+
+}
+
+
+class Basics extends StatefulWidget {
+  const Basics({super.key});
+
+  @override
+  State<Basics> createState() => _BasicsState();
+}
+
+class _BasicsState extends State<Basics> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("Boring", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                FontAwesomeIcons.airbnb,
+                size: 70,
+              )
+          ),
+          Text("Hello Niggas"),
+          Text("Random Number: ${getRandomNumber()}"),
+          ElevatedButton(
+              onPressed: () {},
+              child: const Text("Button")
+          ),
+        ],
       ),
     );
   }
 
   int getRandomNumber(){ return Random().nextInt(1000); }
 }
+
 
 
