475a25878b24c4629bf25800c04ff17c5cbbe842
ugenella <uzziahgenella@gmail.com>
2026-01-22 21:11:16 +0800

    Stateful VS Stateless

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,26 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:course/screens/home.dart';
 
-class Dashboard extends StatelessWidget {
+
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
 
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Dashboard"),
-
-        foregroundColor: Colors.white,
+        title: Text("Dashboard"),
         backgroundColor: Colors.black87,
-        centerTitle: true,
-      ),
+        foregroundColor: Colors.white,
 
-      body: Home()
+      ),
+      body: CustomWidget(),
     );
   }
-
-
 }
+
+class CustomWidget extends StatefulWidget {
+  const CustomWidget({super.key});
+
+  @override
+  State<CustomWidget> createState() => _CustomWidgetState();
+}
+
+class _CustomWidgetState extends State<CustomWidget> {
+
+  bool liked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          title: Text("Hello again"),
+          trailing: IconButton(
+              onPressed: (){
+                setState(() {
+                  liked = !liked;
+                });
+              },
+              icon: liked? (Icon(Icons.star )) : (Icon(Icons.star_border))
+          ),
+        )
+      ],
+    );
+  }
+}
+
 
 
 
