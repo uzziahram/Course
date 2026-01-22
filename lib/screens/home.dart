ee3c07b8863cbf677f4fc461b93504f6c005e242
ugenella <uzziahgenella@gmail.com>
2026-01-22 16:21:26 +0800

    Stack and Positioned Widgets

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,66 +11,33 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        shape: CircleBorder(),
-        onPressed: (){},
-        backgroundColor: Colors.black87,
-        foregroundColor: Colors.yellow,
-        elevation: 0,
-        child: Icon(Icons.add),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.black87,
-        shape: CircularNotchedRectangle(),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.max,
+    return Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            width: 300,
+            height: 300,
+            color: Colors.blue,
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.green,
+            ),
+          ),
+          Positioned(
+            top: 50,
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+          ),
 
-          children: [
-            Padding(
-              padding: EdgeInsetsGeometry.fromLTRB(3, 5, 3, 5),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Icon(Icons.home, color: Colors.white,),
-                  const Text("Home", style: TextStyle(color: Colors.white),)
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsetsGeometry.fromLTRB(5, 5, 15, 5),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Icon(Icons.shopping_cart, color: Colors.white,),
-                  const Text("Shop", style: TextStyle(color: Colors.white))
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsetsGeometry.fromLTRB(15, 5, 1, 5),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Icon(Icons.favorite, color: Colors.white,),
-                  const Text("Fav", style: TextStyle(color: Colors.white))
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsetsGeometry.fromLTRB(1, 5, 0, 5),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Icon(Icons.settings, color: Colors.white,),
-                  const Text("Settings", style: TextStyle(color: Colors.white))
-                ],
-              ),
-            ),
-          ],
-        ),
+        ],
       ),
     );
   }
